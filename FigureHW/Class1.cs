870fe1dcbc0ca556a7c9500cfca13ff5c6573ea1
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-09-20 13:47:02 +0500

    Add project files.

---
+++ FigureHW/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureHW
+{
+    public abstract class Figure
+    {
+        //Площадь
+        public abstract double Area();
+        //Периметр
+        public abstract double Perimeter();
+        public abstract void ToFile();
+    }
+
+    public class Retangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+        public override double Perimeter()
+        {
+            return Width * 2 + Height * 2;
+        }
+        public override void ToFile()
+        {
+            string _path = "resultRetangle.txt";
+            var sw = new StreamWriter(_path, true);
+            sw.WriteLine("Площадь прямоугольника = {0}", this.Area());
+            sw.WriteLine("Периметр прямоугольника = {0}", this.Perimeter());
+            sw.Close();
+        }
+    }
+
+    public class Rhombus : Figure
+    {
+        public double Side { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+
+        public override double Perimeter()
+        {
+            return 4 * Side;
+        }
+        public override void ToFile()
+        {
+            string _path = "resultRhombus.txt";
+            var sw = new StreamWriter(_path, true);
+            sw.WriteLine("Площадь ромба = {0}", this.Area());
+            sw.WriteLine("Периметр ромба = {0}", this.Perimeter());
+            sw.Close();
+        }
+    }
+}
---
+++ FigureHW/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FigureHW;
+using System;
+using System.Drawing;
+
+Console.WriteLine("Выберите фигуру: 1 - Прямоугольник, 2 - Ромб");
+string answer = Console.ReadLine();
+if(answer == "1")
+{
+    Console.WriteLine("Введите высоту и ширину: ");
+    double heightRet = Convert.ToDouble(Console.ReadLine());
+    double widthRet = Convert.ToDouble(Console.ReadLine());
+    var MyRetangle = new Retangle { Width = widthRet, Height = heightRet };
+    Console.WriteLine("Площадь прямоугольника = {0}", MyRetangle.Area());
+    Console.WriteLine("Периметр прямоугольника = {0}", MyRetangle.Perimeter());
+    Console.WriteLine("Сохранить результат в файл: 1 - да, 2 - нет");
+    string answer2 = Console.ReadLine();
+    if (answer2 == "1")
+    {
+        MyRetangle.ToFile();
+    }
+}
+else if(answer == "2")
+{
+    Console.WriteLine("Введите длину стороны ромба, " +
+        "длину основания ромба и длину высоты ромба: ");
+    double sideRom = Convert.ToDouble(Console.ReadLine());
+    double heightRom = Convert.ToDouble(Console.ReadLine());
+    double widthRom = Convert.ToDouble(Console.ReadLine());
+    var MyRhombus = new Rhombus { Side = sideRom, Width = widthRom, Height = heightRom };
+    Console.WriteLine("Площадь ромба = {0}", MyRhombus.Area());
+    Console.WriteLine("Периметр ромба = {0}", MyRhombus.Perimeter());
+    Console.WriteLine("Сохранить результат в файл: 1 - да, 2 - нет");
+    string answer3 = Console.ReadLine();
+    if( answer3 == "1")
+    {
+        MyRhombus.ToFile();
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
